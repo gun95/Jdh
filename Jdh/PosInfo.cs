f43135c244856bdf506de679ae0641fac7d5d2fe
Gun95 <overman95@outlook.com>
2018-10-09 11:33:16 +0200

    Add PosInfo class Add makeJdh with pos Fix jdh color when wave passes

---
+++ Jdh/PosInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ColoreColor = Corale.Colore.Core.Color;
+
+namespace Jdh
+{
+    class PosInfo
+    {
+        public int Row { get; set; }
+        public int Colum { get; set; }
+        public ColoreColor color { get; set; }
+
+        public PosInfo(int row, int colum, ColoreColor color)
+        {
+            this.Row = row;
+            this.Colum = colum;
+            this.color = color;
+        }
+    }
+}
--- Jdh/Program.cs
+++ Jdh/Program.cs
@@ -19,11 +19,109 @@ namespace Jdh
     {
 
 
-        static Corale.Colore.Razer.Keyboard.Key[] KeyJ = { Key.A, Key.Z, Key.E, Key.S, Key.W, Key.A };
+        static Corale.Colore.Razer.Keyboard.Key[] KeyJ = { Key.A, Key.Z, Key.E, Key.S, Key.W };
         static Corale.Colore.Razer.Keyboard.Key[] KeyD = { Key.T, Key.Y, Key.U, Key.G, Key.J, Key.V, Key.B, Key.N };
         static Corale.Colore.Razer.Keyboard.Key[] KeyH = { Key.O, Key.L };
 
         static KeyboardCustom keyboardGrid = KeyboardCustom.Create();
+
+
+        static List<PosInfo> listPosInfo;
+
+
+
+        /*
+        a = 2,2
+        z = 2,3
+        e = 2,4
+        s = 3,3
+        < = 4,2
+        w = 4,3
+
+
+
+        t= 2,6
+        y = 2,7
+        u = 2,8
+        g = 3,6
+        j = 3,8
+        v = 4,6
+        B = 4,7
+        N = 4,8
+
+
+        o = 2,10
+        ^ = 2,12
+        l = 3,10
+        m = 3,11
+        ù = 3,12
+        ; = 4,10
+        ! = 4,12
+           
+    */
+        static void fillList(ColoreColor color)
+        {
+            PosInfo posInfo;
+
+            posInfo = new PosInfo(2, 2, color);
+            listPosInfo.Add(posInfo);
+            posInfo = new PosInfo(2, 3, color);
+            listPosInfo.Add(posInfo);
+            posInfo = new PosInfo(2, 4, color);
+            listPosInfo.Add(posInfo);
+            posInfo = new PosInfo(3, 3, color);
+            listPosInfo.Add(posInfo);
+            posInfo = new PosInfo(4, 2, color);
+            listPosInfo.Add(posInfo);
+            posInfo = new PosInfo(4, 3, color);
+            listPosInfo.Add(posInfo);
+
+
+            posInfo = new PosInfo(2, 6, color);
+            listPosInfo.Add(posInfo);
+            posInfo = new PosInfo(2, 7, color);
+            listPosInfo.Add(posInfo);
+            posInfo = new PosInfo(2, 8, color);
+            listPosInfo.Add(posInfo);
+            posInfo = new PosInfo(3, 6, color);
+            listPosInfo.Add(posInfo);
+            posInfo = new PosInfo(3, 8, color);
+            listPosInfo.Add(posInfo);
+            posInfo = new PosInfo(4, 6, color);
+            listPosInfo.Add(posInfo);
+            posInfo = new PosInfo(4, 7, color);
+            listPosInfo.Add(posInfo);
+            posInfo = new PosInfo(4, 8, color);
+            listPosInfo.Add(posInfo);
+
+
+            posInfo = new PosInfo(2, 10, color);
+            listPosInfo.Add(posInfo);
+            posInfo = new PosInfo(2, 12, color);
+            listPosInfo.Add(posInfo);
+            posInfo = new PosInfo(3, 10, color);
+            listPosInfo.Add(posInfo);
+            posInfo = new PosInfo(3, 11, color);
+            listPosInfo.Add(posInfo);
+            posInfo = new PosInfo(3, 12, color);
+            listPosInfo.Add(posInfo);
+            posInfo = new PosInfo(4, 10, color);
+            listPosInfo.Add(posInfo);
+            posInfo = new PosInfo(4, 12, color);
+            listPosInfo.Add(posInfo);
+        }
+
+        static void MakeJdh()
+        {
+            int i = 0;
+
+            while (i < listPosInfo.Count)
+            {
+                keyboardGrid[listPosInfo[i].Row, listPosInfo[i].Colum] = listPosInfo[i].color;
+                i++;
+            }
+        }
+
 
         // Create a custom for the Keyboard
         // Set the A Key to color
@@ -42,7 +140,7 @@ namespace Jdh
         // Set the D to color
         static void MakeD(ColoreColor color)
         {
-            
+
             int i = 0;
             while (i < KeyD.Length)
             {
@@ -78,30 +176,32 @@ namespace Jdh
             Chroma.Instance.Keyboard.SetCustom(keyboardGrid);
         }
 
-        static void Wave(Key keyUp, Key keyDown, ColoreColor color)
+        static ColoreColor CheckListInfo(int rowKeyDown, int ColumKeyDown)
         {
-            if (keyUp != Key.Invalid)
-                Chroma.Instance.Keyboard.SetKey(keyUp, color);
+            int i = 0;
 
-            if (keyDown != Key.Invalid)
-                Chroma.Instance.Keyboard.SetKey(keyDown, ColoreColor.Black);
-
-            System.Threading.Thread.Sleep(100);
+            while (i < listPosInfo.Count)
+            {
+                if (listPosInfo[i].Row == rowKeyDown && listPosInfo[i].Colum == ColumKeyDown)
+                    return listPosInfo[i].color;
+                i++;
+            }
+            return ColoreColor.Black;
         }
 
         static void Wave(int rowKeyUp, int columKeyUp, int rowKeyDown, int ColumKeyDown, Semaphore semaphoreObject, ColoreColor color)
         {
-            if (rowKeyUp != -1 && columKeyUp != -1 && Chroma.Instance.Keyboard[rowKeyUp, columKeyUp] == ColoreColor.Black)
+            if (rowKeyUp != -1 && columKeyUp != -1)
             {
                 semaphoreObject.WaitOne();
                 Chroma.Instance.Keyboard.SetPosition(rowKeyUp, columKeyUp, color);
                 semaphoreObject.Release();
             }
 
-            if (rowKeyDown != -1 && ColumKeyDown != -1 && Chroma.Instance.Keyboard[rowKeyDown, ColumKeyDown] == color)
+            if (rowKeyDown != -1 && ColumKeyDown != -1)
             {
                 semaphoreObject.WaitOne();
-                Chroma.Instance.Keyboard.SetPosition(rowKeyDown, ColumKeyDown, ColoreColor.Black);
+                Chroma.Instance.Keyboard.SetPosition(rowKeyDown, ColumKeyDown, CheckListInfo(rowKeyDown, ColumKeyDown));
                 semaphoreObject.Release();
             }
             System.Threading.Thread.Sleep(100);
@@ -163,14 +263,14 @@ namespace Jdh
 
         static void Main(string[] args)
         {
-            Semaphore semaphoreObject = new Semaphore(initialCount: 1, maximumCount: 1, name: "Jdf");
+            Semaphore semaphoreObject = new Semaphore(initialCount: 1, maximumCount: 1, name: "Jdh");
             List<Dictionary<int, int>> mapLock = new List<Dictionary<int, int>>();
 
             System.Diagnostics.Debug.WriteLine(Chroma.Instance.Initialized);
             System.Threading.Thread.Sleep(1000);
 
 
-       
+
 
             int p = 0;
             while (p < 5)
@@ -257,7 +357,7 @@ namespace Jdh
                 i = 0;
                 //PrintJDH(ColoreColor.White);
 
-               // System.Threading.Thread.Sleep(10000);
+                // System.Threading.Thread.Sleep(10000);
             }
         }
     }
